39ac90990784cb7b505a3fedfb5716c8602ab297
Antony García <antony.garcia.gonzalez@gmail.com>
2023-08-22 21:31:55 -0400

    Added Random Forest

---
+++ ciemi/scipts/random_forest_main.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+# Cargar los datos desde los archivos CSV
+train_data = pd.read_csv("../datasets/mnist/mnist_train.csv")
+test_data = pd.read_csv("../datasets/mnist/mnist_test.csv")
+
+# Separar las etiquetas de los datos
+X_train = train_data.iloc[:, 1:].values
+y_train = train_data.iloc[:, 0].values
+X_test = test_data.iloc[:, 1:].values
+y_test = test_data.iloc[:, 0].values
+
+# Normalizar los datos dividiendo por 255
+X_train_normalized = X_train / 255.0
+X_test_normalized = X_test / 255.0
+
+# Crear un clasificador Random Forest con 100 árboles (puedes ajustar este valor)
+random_forest_classifier = RandomForestClassifier()
+
+# Entrenar el clasificador Random Forest con los datos de entrenamiento normalizados
+random_forest_classifier.fit(X_train_normalized, y_train)
+
+# Realizar predicciones en el conjunto de prueba normalizado
+y_test_pred = random_forest_classifier.predict(X_test_normalized)
+
+# Calcular la precisión en el conjunto de prueba
+accuracy = accuracy_score(y_test, y_test_pred)
+print("Accuracy en el conjunto de prueba:", accuracy)
